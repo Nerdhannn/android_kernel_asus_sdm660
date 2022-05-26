d29f953adbacd23aff29a2f7bb3e2b0e63158c3b
Nerdhannn <ramadhananggadev@gmail.com>
2022-05-26 14:56:38 +0000

    [SQUASH]{X00TD,X01BD}_defconfig: Disable many governor and iosched

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -541,59 +541,7 @@ extern struct cpufreq_governor cpufreq_gov_sched;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHEDUTIL)
 extern struct cpufreq_governor cpufreq_gov_schedutil;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_schedutil)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CHILL)
-extern struct cpufreq_governor cpufreq_gov_chill;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_chill)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLU_ACTIVE)
-extern struct cpufreq_governor cpufreq_gov_blu_active;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_blu_active)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_NEBULA)
-extern struct cpufreq_governor cpufreq_gov_nebula;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_nebula)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVEX)
-extern struct cpufreq_governor cpufreq_gov_interactivex;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactivex)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIACTIVE)
-extern struct cpufreq_governor cpufreq_gov_intelliactive;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_intelliactive)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKNESSV5)
-extern struct cpufreq_governor cpufreq_gov_darknessv5;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_darknessv5)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
-extern struct cpufreq_governor cpufreq_gov_zzmoove;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_zzmoove)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ALUCARD)
-extern struct cpufreq_governor cpufreq_gov_alucard;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_alucard)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_NIGHTMARE)
-extern struct cpufreq_governor cpufreq_gov_nightmare;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_nightmare)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLU_SCHEDUTIL)
-extern struct cpufreq_governor cpufreq_gov_blu_schedutil;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_blu_schedutil)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIONHEART)
-extern struct cpufreq_governor cpufreq_gov_lionheart;
-#define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_LIONHEART)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKUTIL)
-extern struct cpufreq_governor cpufreq_gov_darkutil;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_darkutil)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ELECTROUTIL)
-extern struct cpufreq_governor cpufreq_gov_electroutil;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_electroutil)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HELIX)
-extern struct cpufreq_governor cpufreq_gov_helix_schedutil;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_helix_schedutil)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKNESSSCHED)
-extern struct cpufreq_governor cpufreq_gov_darknesssched;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_darknesssched)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ENERGY)
-extern struct cpufreq_governor cpufreq_gov_energy_dcfc;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_energy_dcfc)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHEDHORIZON)
-extern struct cpufreq_governor cpufreq_gov_schedhorizon;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_schedhorizon)
 #endif
-
 
 static inline void cpufreq_policy_apply_limits(struct cpufreq_policy *policy)
 {
